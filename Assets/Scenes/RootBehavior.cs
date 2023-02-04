3a08b5c750f2f2be4a3f433336a338cd04ef26fc
AREFYTON <axelle.refyton@ynov.com>
2023-02-04 12:02:03 +0100

    roots doesn't bounce back anymore

--- Assets/Scenes/RootBehavior.cs
+++ Assets/Scenes/RootBehavior.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
-
 public class RootBehavior : MonoBehaviour
 {
     [Range(0, 100)]
     public float Progression = 0;
-    public float ProgressionSpeed = 0.1f;
+    public float ProgressionSpeed = 1;
     public int InitialScale = 20;
+    public bool Reduce;
+    
     // Start is called before the first frame update
-    void Start(){}
+    void Start()
+    {
+        Reduce = true;
+    }
 
     // Update is called once per frame
     void Update()
-    {
-        Progression+=ProgressionSpeed;        
-        transform.localScale = new Vector3(InitialScale - InitialScale * Progression * 0.01f,InitialScale - InitialScale * Progression * 0.01f, 1);
+    { 
+        if (Progression >= 99) 
+        {
+            Reduce = false;
+        }
+
+        if (Reduce)
+        {
+            Progression+=ProgressionSpeed * 1;        
+            transform.localScale = new Vector3(InitialScale - InitialScale * Progression * 0.01f,InitialScale - InitialScale * Progression * 0.01f, 1);
+        }
     }
 }
