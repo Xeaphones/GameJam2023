c902adcd202831a7d5a42ce15aebdcc638b84475
YVelay <yohan.velay@ynov.com>
2023-02-05 17:30:16 +0100

    Anim done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
-
     private Rigidbody2D rb;
+    private string direction = "down";
 
     void Start()
     {
@@ -17,5 +17,48 @@ public class PlayerController : MonoBehaviour
 
         Vector2 movement = new Vector2(horizontal, vertical);
         rb.velocity = movement * speed;
+
+        if (horizontal == 0 && vertical == 0)
+        {
+            switch (direction)
+            {
+                case "up":
+                    GetComponent<Animator>().Play("PlayerIdleUp");
+                    break;
+                case "down":
+                    GetComponent<Animator>().Play("PlayerIdle");
+                    break;
+                case "right":
+                    GetComponent<Animator>().Play("PlayerIdleRight");
+                    break;
+                case "left":
+                    GetComponent<Animator>().Play("PlayerIdleLeft");
+                    break;
+            }
+        }
+        else if (vertical > 0)
+        {
+            // change animation to up
+            GetComponent<Animator>().Play("PlayerWalkUp");
+            direction = "up";
+        }
+        else if (vertical < 0)
+        {
+            // change animation to down
+            GetComponent<Animator>().Play("PlayerWalkDown");
+            direction = "down";
+        }
+        else if (horizontal > 0) 
+        {
+            // change animation to right
+            GetComponent<Animator>().Play("PlayerWalkRight");
+            direction = "right";
+        }
+        else if (horizontal < 0)
+        {
+            // change animation to left
+            GetComponent<Animator>().Play("PlayerWalkLeft");
+            direction = "left";
+        }
     }
 }
